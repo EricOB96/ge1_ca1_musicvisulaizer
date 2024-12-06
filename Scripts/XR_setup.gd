19123b5d4dadf516f305f0cc83b6448406bcc704
Eric O'Brien <ericobrien246@gmail.com>
2024-12-06 13:14:50 +0000

    Project setup
    XR setup, main scene as well as player set up

--- Scripts/XR_setup.gd
+++ Scripts/XR_setup.gd
@@ -1,11 +1,16 @@
-extends XROrigin3D
+extends Node3D
 
+var xr_interface: XRInterface
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
 
+		# Turn off v-sync!
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		# Change our main viewport to output to the HMD
+		get_viewport().use_xr = true
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
